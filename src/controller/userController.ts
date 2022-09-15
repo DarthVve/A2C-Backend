95d8c949c86fda8d0384644963d0fa045dc141c8
DarthVve <oshevire.majoroh@decagon.dev>
2022-09-15 17:56:19 +0100

    develop: formatting after Forgot-Password commit 1.0.4

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -47,14 +47,14 @@ export async function registerUser(req: Request, res: Response) {
     });
 
     if (user) {
-      const html =  emailVerificationView(id)
+      const html = emailVerificationView(id)
 
       await mailer.sendEmail(
         appEmail, req.body.email, "please verify your email", html
       )
     }
-    else{
-      res.status(403).json({ msg:'Verification mail failed to send', user });
+    else {
+      res.status(403).json({ msg: 'Verification mail failed to send', user });
     }
     res.status(201).json({ msg: 'User created successfully' });
   } catch (err) {
@@ -69,7 +69,7 @@ export async function loginUser(req: Request, res: Response) {
   try {
     const validationResult = loginSchema.validate(req.body, options);
     if (validationResult.error) {
-      return res.status(400).json({ Error: validationResult.error.details[0].message});
+      return res.status(400).json({ Error: validationResult.error.details[0].message });
     }
     const user = await UserInstance.findOne({
       where: {
@@ -123,65 +123,63 @@ export async function verifyUser(req: Request, res: Response) {
     const user = await UserInstance.findOne({ where: { id: id } });
 
     if (user) {
-      const updateVerified = await user.update({
-        verified:true
-      })
-      if(updateVerified){
-        res.status(200).json({ msg:'User verified', updateVerified })
+      const updateVerified = await user.update({ verified: true });
+      if (updateVerified) {
+        res.status(200).json({ msg: 'User verified', updateVerified })
       }
     } else {
-      res.status(404).json({ msg:'Verification failed' });
+      res.status(404).json({ msg: 'Verification failed' });
     }
-    
+
   } catch (err) {
-  console.error(err);
-  res.status(500).json({ message: 'not verified', route: 'verify/id' });
+    console.error(err);
+    res.status(500).json({ message: 'not verified', route: 'verify/id' });
   }
 }
 
 
-//Password Reset, Sends an email 
-export async function forgetPassword ( req: Request, res: Response ) {
+//Password Reset, Sends an email
+export async function forgetPassword(req: Request, res: Response) {
   try {
-      const { email } = req.body
-      const user = await UserInstance.findOne({ where: { email: email } }) as unknown as {[key:string]:string} as any
-     
-      if (user) {
-        const { id } = user;
-        const html = passwordMailTemplate(id);
-        const subject = "New Account Password";
-        await mailer.sendEmail("AirtimeToCash", email, subject, html);
-        res.status(200).json({ msg:"new password sent" });
-      } else {
-          res.status(400).json({msg:"invalid email Address"});
-      }
+    const { email } = req.body
+    const user = await UserInstance.findOne({ where: { email: email } }) as unknown as { [key: string]: string } as any
+
+    if (user) {
+      const { id } = user;
+      const html = passwordMailTemplate(id);
+      const subject = "New Account Password";
+      await mailer.sendEmail("AirtimeToCash", email, subject, html);
+      res.status(200).json({ msg: "new password sent" });
+    } else {
+      res.status(400).json({ msg: "invalid email Address" });
+    }
   } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Failed to send password', route: '/forgetPassword' });
+    console.error(err);
+    res.status(500).json({ message: 'Failed to send password', route: '/forgetPassword' });
   }
 }
 
 
 //User password update
-export async function resetPassword(req:Request, res:Response) {
+export async function resetPassword(req: Request, res: Response) {
   try {
-      const { id } = req.params
-      const { password } = req.body
-      const user = await UserInstance.findOne({ where: { id: id } }) 
-      if (user) {
-          const passwordHash = await bcrypt.hash(password, 8)
-          let updatePassword = await user.update({ password:passwordHash });
+    const { id } = req.params
+    const { password } = req.body
+    const user = await UserInstance.findOne({ where: { id: id } })
+    if (user) {
+      const passwordHash = await bcrypt.hash(password, 8)
+      let updatePassword = await user.update({ password: passwordHash });
 
-          if (updatePassword) {
-            res.status(200).json({ msg:"password successfully updated" });
-          } else {
-            res.status(400).json({ msg:"failed to update password" });
-          }
+      if (updatePassword) {
+        res.status(200).json({ msg: "password successfully updated" });
+      } else {
+        res.status(400).json({ msg: "failed to update password" });
       }
+    }
   } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Failed to reset password', route: '/resetPassword' });
+    console.error(err);
+    res.status(500).json({ message: 'Failed to reset password', route: '/resetPassword' });
   }
 };
-  
+
 
