0d3d8b8a09f71a8d583aa7fa0ed41b020bd4b6dc
tofarati <oluwatofarati.agbaje@decagon.dev>
2022-10-13 04:28:03 +0100

    2fa: secondAuth, node-2fa, vonage, protected confirm/cancel transaction, protected credit wallet, 2fa email  2.0

--- src/controller/adminController.ts
+++ src/controller/adminController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { UserInstance } from "../model/userModel";
+import { isValid2FA } from "../utility/twoFactorAuth";
 
 export async function elevateToAdmin(req: Request, res: Response) {
   try {
@@ -31,5 +32,20 @@ export async function revokeAdmin(req: Request, res: Response) {
   } catch (err) {
     console.error(err);
     res.status(500).json({ msg: 'failed to revoke admin privileges', route: '/admin/revoke/'});
+  }
+}
+
+export async function check2FAToken(req: Request, res: Response) {
+  try {
+    const { sessionCode } = req.body;
+    if (isValid2FA(sessionCode as string)) {
+      res.cookie('sessionCode', sessionCode, { maxAge: 30 * 60 * 1000, httpOnly: true });
+      return res.status(200).json({ msg: "2FA token verified" });
+    } else {
+      return res.status(401).json({ msg: "Invalid 2FA token" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: 'failed to generate 2FA token', route: '/admin/authenticate' });
   }
 }
