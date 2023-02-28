11b19bd66f33c3c847077b4489aeaa7023c68666
Darth Vve <osheviremajoroh@gmail.com>
2023-02-28 21:40:13 +0100

    Stable and Deployed to Fly.io

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -9,11 +9,11 @@ const secret = process.env.JWT_SECRET as string;
 export async function auth(req: Request, res: Response, next: NextFunction) {
   try {
     const authorization = req.headers.authorization;
-    if (!authorization && !req.cookies.token) {
+    const token = authorization?.slice(7) || req.cookies.token as string;
+    if (token) {
       return res.status(401).json({ msg: "Authentication required. Please login" })
     }
 
-    const token = authorization?.slice(7) || req.cookies.token as string;
     const verified = verify(token, secret);
     if (!verified) {
       return res.status(401).json({ msg: "Token expired/invalid. Please login" });
